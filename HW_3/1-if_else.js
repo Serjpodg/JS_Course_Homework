561c8cb45d04f8fddb4b06092af0cd6cd6190c9c
Serjpodg <32710770+Serjpodg@users.noreply.github.com>
2017-10-27 19:27:12 +0300

    Add files via upload

---
+++ HW_3/1-if_else.js
@@ -0,0 +1,55 @@
+/*//Task 1
+var a = +prompt("enter a");
+if (a == 0) {
+    console.log("Верно!");
+}
+else {
+    console.log("Неверно!");
+}
+
+//Task2
+var a = prompt("enter a");
+if (a == 'test') {
+    console.log("Верно!");
+}
+else {
+    console.log("Неверно!");
+}
+
+//Task 3.1
+function testTrue(test) {
+    if (test === true) {
+        console.log("Верно!");
+    }
+    else {
+        console.log("Неверно!");
+    }
+}
+
+testTrue(true);
+testTrue(false);
+
+//Task 3.2
+function testTrue1(test1) {
+    test1 === true ? console.log("Верно!") : console.log("Неверно!");
+}
+
+testTrue1(true);
+testTrue1(false);
+
+//Task 4
+var a = +prompt("enter a");
+var b = +prompt("enter b");
+var result = a + b;
+if (result > 5) {
+    result = 5
+}
+else {
+    result *= 10;
+}
+
+console.log(result); */
+
+//Task 5
+var a = +prompt("enter a");
+var b = +prompt("enter b");
