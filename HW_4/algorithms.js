1c1a22d1ba05288bd2f91389de98e022d33afb4f
Serjpodg <32710770+Serjpodg@users.noreply.github.com>
2017-11-08 12:22:19 +0200

    Update algorithms.js

--- HW_4/algorithms.js
+++ HW_4/algorithms.js
@@ -1,1 +1,30 @@
 //1
+function isPrime(x) {
+    if ( x !== 1 && x <= 3) {
+        return 1;
+    } else {
+        for (var i = 2; i < x; i++) {
+            if (x % i === 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+var arrPrime = [];
+function primesInRange(a,b) {
+    for (var i = a, j = 0; i <= b; i++) {
+        if (isPrime(i)) {
+            arrPrime[j] = i;
+            j++;
+        }
+    }
+    return arrPrime;
+}
+
+alert(primesInRange(+prompt("x"),+prompt("y")));
+
+//2
+
+
