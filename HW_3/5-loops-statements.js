7ee769165cfd6b1ba3fa6c28f383f9cf5b5dc00c
Serjpodg <32710770+Serjpodg@users.noreply.github.com>
2017-11-19 19:47:00 +0200

    Update 5-loops-statements.js

--- HW_3/5-loops-statements.js
+++ HW_3/5-loops-statements.js
@@ -16,3 +16,44 @@ function evenodd (x) {
 }
 
 evenodd(15);
+
+//Task 2
+
+function FizzBuzz (x) {
+
+    var f = "Fizz";
+    var b = "Buzz";
+    var j = 0;
+
+    for (var i = 0; i <= x; i++) {
+        if (i % 15 === 0) {j = f+b;}
+        else if (i % 3 === 0) {j = f;}
+        else if (i % 5 === 0) {j = b;}
+        else {j = i;}
+        console.log(j);
+    }
+}
+
+FizzBuzz(100);
+
+//Task 3
+function GCD (x, y) {
+
+    var a = 0;
+    var b = 0;
+    var gcd;
+
+    if (x >= y) {a = y; b = x;} else {a = x; b = y;}
+
+    for (var i = a; i > 0; i--) {
+        if ( (b % i === 0) && (a % i === 0) ) {
+            gcd = i;
+            alert(gcd);
+            return;
+        }
+    }
+}
+
+GCD(256, 128);
+
+//Task 4
